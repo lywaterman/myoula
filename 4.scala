867cd5fddf3b53560e695b97bf8674986693ea03
Robert Liu <liuyang124846@gmail.com>
2013-10-28 23:39:52 +0800

    a more nice look

--- 4.scala
+++ 4.scala
@@ -1,33 +1,14 @@
 //暴力解法
-var l1 = (100 to 999).toList
-var l2 = (100 to 999).toList
-
-var l3 = for (v1 <- l1; v2 <- l2) yield v1 * v2
-
 def isPalindrome(num: Int) : Boolean = {
   var numStr = num.toString()
-  var numStrLen = numStr.length
 
-  if (numStrLen % 2 == 0) {
-    var count = numStrLen / 2
-    var dropStr = numStr.drop(count)
-    var dropRStr = numStr.dropRight(count)
-
-    dropStr == dropRStr.reverse
-
-  } else {
-    var count = (numStrLen / 2).toInt
-    
-    var dropStr = numStr.drop(count)
-    var dropRStr = numStr.dropRight(count)
-
-    dropStr == dropRStr.reverse
-  }
+  numStr == numStr.reverse
 }
 
-var Palindromes = for (v3 <- l3; if isPalindrome(v3)) yield v3 
+var finalNum = (for (v1 <- (100 to 999);
+                    v2 <- (100 to 999);
+                    v3 = v1 * v2;
+                    if isPalindrome(v3)) yield v3).max
 
-Palindromes = Palindromes.sorted
-
-println(Palindromes(Palindromes.size - 1))
+println(finalNum)
     
